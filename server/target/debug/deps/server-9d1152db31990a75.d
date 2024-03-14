8daecabb9d9c50711e74b5ffec77b3ac35c3e4bc
mtmattqq <mtmattqq@gmail.com>
2024-03-14 15:58:31 +0800

    refer to offcial document

---
+++ server/target/debug/deps/server-9d1152db31990a75.d
@@ -0,0 +1,5 @@
+/home/user/Programming/CLI_OnlineJudge/server/target/debug/deps/libserver-9d1152db31990a75.rmeta: src/lib.rs
+
+/home/user/Programming/CLI_OnlineJudge/server/target/debug/deps/server-9d1152db31990a75.d: src/lib.rs
+
+src/lib.rs:
---
+++ server/target/debug/deps/server-ce6081fb8245210e.d
@@ -0,0 +1,5 @@
+/home/user/Programming/CLI_OnlineJudge/server/target/debug/deps/libserver-ce6081fb8245210e.rmeta: src/main.rs
+
+/home/user/Programming/CLI_OnlineJudge/server/target/debug/deps/server-ce6081fb8245210e.d: src/main.rs
+
+src/main.rs:
