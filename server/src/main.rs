4131622384c7f4cf0b96a39354371048885608af
mtmattqq <mtmattqq@gmail.com>
2024-03-15 19:04:15 +0800

    add git ignore and remove target

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,16 +7,16 @@ use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 use rand::thread_rng;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8787").unwrap();
-    let pool = ThreadPool::new(4);
+    // let listener = TcpListener::bind("127.0.0.1:8787").unwrap();
+    // let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    // for stream in listener.incoming() {
+    //     let stream = stream.unwrap();
 
-        pool.execute(|| {
-            handle_connection(stream);
-        });
-    }
+    //     pool.execute(|| {
+    //         handle_connection(stream);
+    //     });
+    // }
 
     let mut rng = thread_rng();
     let bits = 2048;
