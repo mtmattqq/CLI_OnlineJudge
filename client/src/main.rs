f9bdbcf84424ca10f7eb68621b3cc79ca0eb9d18
mtmattqq <mtmattqq@gmail.com>
2024-03-17 19:11:11 +0800

    Transfer data with rsa encryption

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,9 @@
-use std::alloc::handle_alloc_error;
+use std::fs;
 use std::net::TcpStream;
 use std::io::{Read, Write};
+use rsa::pkcs1::DecodeRsaPublicKey;
+use rsa::{Pkcs1v15Encrypt, RsaPublicKey};
+use rand::thread_rng;
 
 fn main() {
     let tcp_stream = TcpStream::connect("127.0.0.1:8787");
@@ -17,13 +20,24 @@ fn main() {
 fn handle_connect(mut stream: TcpStream) {
     println!("Successfully connected to server");
 
-    let msg = b"Hello!";
+    let mut pub_key: [u8; 426] = [0; 426];
 
-    stream.write_all(msg).unwrap();
+    match stream.read_exact(&mut pub_key) {
+        Ok(_) => println!("Successfully read public key."),
+        Err(e) => eprintln!("Failed to read public key: {}", e),
+    }
+
+    let pub_key = String::from_utf8(pub_key.to_vec()).unwrap();
+    let pub_key = RsaPublicKey::from_pkcs1_pem(pub_key.as_str()).unwrap();
+
+    let mut rng = thread_rng();
+    let msg = fs::read_to_string("main.c").unwrap();
+    let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &msg.as_bytes()).expect("Encrypt Failed");
+
+    stream.write_all(&enc_data).unwrap();
     stream.flush().unwrap();
-    println!("Sent Hello, awaiting reply...");
-    
     stream.shutdown(std::net::Shutdown::Write).unwrap();
+    println!("Sent Hello, awaiting reply...");
 
     let mut data: String = String::new(); // using 6 byte buffer
     match stream.read_to_string(&mut data) {
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,43 +3,46 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+use rsa::{
+    Pkcs1v15Encrypt, 
+    RsaPrivateKey, 
+    RsaPublicKey, 
+    pkcs1::EncodeRsaPublicKey,
+    pkcs1::LineEnding
+};
 use rand::thread_rng;
 
 fn main() {
-    // let listener = TcpListener::bind("127.0.0.1:8787").unwrap();
-    // let pool = ThreadPool::new(4);
+    let listener = TcpListener::bind("127.0.0.1:8787").unwrap();
+    let pool = ThreadPool::new(4);
 
-    // for stream in listener.incoming() {
-    //     let stream = stream.unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-    //     pool.execute(|| {
-    //         handle_connection(stream);
-    //     });
-    // }
-
-    let mut rng = thread_rng();
-    let bits = 2048;
-    let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("Fail to Create Key");
-    let pub_key = RsaPublicKey::from(&priv_key);
-
-    let data = b"hello world";
-
-    let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &data[..]).expect("Encrypt Failed");
-    assert_ne!(&data[..], &enc_data[..]);
-
-    let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("Decrypt Failed");
-    assert_eq!(&data[..], &dec_data[..]);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
 
     println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    let mut rng = thread_rng();
+    let bits = 2048;
+    let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("Fail to Create Key");
+    let pub_key = RsaPublicKey::from(&priv_key);
+
+    stream.write_all(pub_key.to_pkcs1_pem(LineEnding::LF).unwrap().as_bytes()).unwrap();
+    stream.flush().unwrap();
+
     let mut buffer:Vec<u8> = Vec::new();
     stream.read_to_end(&mut buffer).unwrap();
 
-    let buffer = String::from_utf8(buffer).unwrap();
-    println!("{}", buffer);
+    let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &buffer).expect("Decrypt Failed");
+    let dec_data = String::from_utf8(dec_data).unwrap();
+
+    fs::write("Solve.c", dec_data).unwrap();
 
     let filename = "output.info";
 
